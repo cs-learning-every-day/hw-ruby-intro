bbd0afb6e6570545cbb443890c3cf332afd7ed3c
Tesla <1394466835@qq.com>
2023-01-19 09:19:57 +0800

    feat: finish part 3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -55,14 +55,22 @@ end
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant?(s)
   # YOUR CODE HERE
+  (s =~ /^[^aeiou\W]/i) != nil
 end
 
 def binary_multiple_of_4?(s)
   # YOUR CODE HERE
+  m = s.match(/[01]+/)
+  if m == nil || m[0].length != s.length
+    return false
+  end
+
+  return s.to_i(2) % 4 == 0
 end
 
 # Part 3
